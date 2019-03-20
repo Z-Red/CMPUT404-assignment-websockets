52abcb3f0bd9b56bb7700d4bb34cd44f96fe0be7
Z-Red <araien@ualberta.ca>
2019-03-20 10:19:00 -0600

    Transfer Flask related code from AJAX assignment (A4)

--- sockets.py
+++ sockets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright (c) 2013-2014 Abram Hindle
+# Copyright (c) 2013-2014 Abram Hindle, Araien Zach Redfern
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -22,7 +22,7 @@ import time
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 sockets = Sockets(app)
 app.debug = True
 
@@ -31,7 +31,7 @@ class World:
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+
     def add_set_listener(self, listener):
         self.listeners.append( listener )
 
@@ -55,27 +55,34 @@ class World:
 
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+
     def world(self):
         return self.space
 
-myWorld = World()        
+myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
 
 myWorld.add_set_listener( set_listener )
-        
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return app.send_static_file('index.html')
 
+# TODO:
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
+
+
+    # TODO: Read from websocket
+
+    # TODO: Update the world
     return None
 
+# TODO:
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
@@ -99,23 +106,34 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
 
-@app.route("/world", methods=['POST','GET'])    
+    json_data = json.loads(request.data.decode("utf8"))
+    for k, v in json_data.items():
+        myWorld.update(entity, k, v)
+
+    return Response(json.dumps(myWorld.get(entity)), status=200, mimetype='application/json')
+
+@app.route("/world", methods=['POST','GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    return Response(json.dumps(myWorld.world()), status=200, mimetype='application/json')
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+
+    e = myWorld.get(entity)
+    if e == None:
+        return Response(json.dumps(e), status=404, mimetype='application/json')
+    else:
+        return Response(json.dumps(e), status=200, mimetype='application/json')
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return Response(json.dumps(myWorld.world()), status=200, mimetype='application/json')
 
 
 
